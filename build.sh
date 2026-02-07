8c1d8e13fa810b9c4a6ce08c5d4b37b0f1821f0a
Nikolay Kharchenko <nick@kharchenko.net>
2026-02-07 21:12:13 +0500

    Add build.sh for building ipa with embedded version

---
+++ build.sh
@@ -0,0 +1,10 @@
+# Check for version number parameter
+[[ "$1" =~ "^[0-9]+$" ]] || { echo "Invalid version number"; exit 1; }
+
+dotnet publish UI/UI.csproj \
+	-c Release \
+	-f net10.0-ios \
+	/p:BuildIpa=true \
+	/p:RunAOTCompilation=false \
+	/p:PublishTrimmed=true \
+	/p:ApplicationVersion=$1
